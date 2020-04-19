a4aac2d9aaab75d8b35eb5c1ab171084c9e352b4
supercut9(yamiao liu) <ymliu97@gmail.com>
2020-04-18 22:25:17 -0400

    add Restart button/shortkey

--- Week9-CodeLab1-GridGame/Assets/Scripts/Restart.cs
+++ Week9-CodeLab1-GridGame/Assets/Scripts/Restart.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class Restart : MonoBehaviour
 {
     public void RestartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
 }
